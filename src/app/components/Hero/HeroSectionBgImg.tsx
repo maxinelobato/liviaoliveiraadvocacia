9c0583983a4618aa6303279ea2579bdc935868a6
maxinelobato <maxinelobato89@hotmail.com>
2024-05-05 19:04:29 -0300

    changes

--- src/app/components/Hero/HeroSectionBgImg.tsx
+++ src/app/components/Hero/HeroSectionBgImg.tsx
@@ -6,7 +6,9 @@ export function HeroSectionBgImg() {
       <Image
         src="https://images.unsplash.com/photo-1634328724811-c20d433c3358?q=750&w=1080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt="BgImage"
-        sizes="100vw"
+        sizes='"(max-width: 768px) 100vw,
+        (max-width: 1200px) 50vw,
+        33vw"'
         priority
         fill
         className="h-auto w-full bg-cover bg-fixed bg-center bg-no-repeat object-cover"
