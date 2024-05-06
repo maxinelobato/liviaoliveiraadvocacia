de36628bf298605710db23e5284d7b7273305aa4
maxinelobato <maxinelobato89@hotmail.com>
2024-05-06 18:07:32 -0300

    changes

--- src/app/components/Hero/HeroSectionBgImg.tsx
+++ src/app/components/Hero/HeroSectionBgImg.tsx
@@ -1,18 +1,18 @@
 import Image from 'next/image'
-import bgImage from '../../../../public/bgImage.avif'
+// import bgImage from '../../../../public/bgImage.avif'
 
 export function HeroSectionBgImg() {
   return (
     <>
       <Image
-        src={bgImage}
+        src="https://images.unsplash.com/photo-1634328724811-c20d433c3358?q=1024&w=1024&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt="BgImage"
         sizes="100vw"
         priority
         fill
-        className="h-80 w-full bg-cover bg-fixed bg-center bg-no-repeat object-cover"
+        className="h-auto w-full bg-cover bg-fixed bg-center bg-no-repeat object-cover"
       />
-      <div className="absolute h-full w-full bg-gradient-to-r from-brandBlue900/90 from-40% to-brandBlue500/45 to-90% p-6 md:p-12 lg:px-16 lg:py-12"></div>
+      <div className="absolute h-full w-full bg-gradient-to-r from-brandBlue900/90 from-20% to-brandBlue500/45 to-70% p-6 md:p-12 lg:px-16 lg:py-12"></div>
     </>
   )
 }
