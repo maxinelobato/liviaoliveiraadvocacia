ba24d042b519891aa450bcc567523ed6da0dee45
maxinelobato <maxinelobato89@hotmail.com>
2024-05-07 02:10:27 -0300

    changes

--- src/app/components/Image/index.tsx
+++ src/app/components/Image/index.tsx
@@ -18,6 +18,8 @@ export function ImgBgImage() {
       <Image
         src="https://images.unsplash.com/photo-1634328724811-c20d433c3358?q=80&w=1080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt="BgImage"
+        width={500}
+        height={500}
         sizes='"(max-width: 768px) 100vw,
         (max-width: 1200px) 50vw,
         33vw"'
