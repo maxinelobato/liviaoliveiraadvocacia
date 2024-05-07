a68f4cd8cfaff8da2c935ddceb067d2462e1b45e
maxinelobato <maxinelobato89@hotmail.com>
2024-05-07 01:11:43 -0300

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,13 @@ const nextConfig = {
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     formats: ['image/avif', 'image/webp'],
+    minimumCacheTTL: 60,
     remotePatterns: [
       {
         protocol: 'https',
         hostname: '**',
       },
     ],
-    // loader: 'cloudinary',
-    // path: 'https://res.cloudinary.com/dibhc6pw4/image/upload/v1712887924/lfag-client',
   },
 }
 export default nextConfig
