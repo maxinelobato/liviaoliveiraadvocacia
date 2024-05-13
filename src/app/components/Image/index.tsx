1efcc8194bcf53417e834541254ddd4c3ea61033
maxinelobato <maxinelobato89@hotmail.com>
2024-05-13 20:26:14 -0300

    changes

--- src/app/components/Image/index.tsx
+++ src/app/components/Image/index.tsx
@@ -5,7 +5,7 @@ import AboutLivia from '../../../../public/img/img3.webp'
 export function ImgBgImage() {
   return (
     <Image
-      src="https://images.unsplash.com/photo-1634328724811-c20d433c3358?q=80&w=1080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      src="https://images.unsplash.com/photo-1634328724811-c20d433c3358?q=80&w=1024&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       alt="BgImage"
       sizes="100vw"
       priority
@@ -24,6 +24,7 @@ export function ImgLogoLivia() {
         width={180}
         height={100}
         loading="lazy"
+        rel="preload"
         className="w-48 md:w-auto lg:w-52"
         sizes='"(max-width: 768px) 100vw,
           (max-width: 1200px) 50vw,
