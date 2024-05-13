66b08c5490ed4235240fe18e6dc06e634f6572e1
maxinelobato <maxinelobato89@hotmail.com>
2024-05-13 17:29:17 -0300

    changes

--- src/app/components/Image/index.tsx
+++ src/app/components/Image/index.tsx
@@ -4,30 +4,16 @@ import AboutLivia from '../../../../public/img/img3.webp'
 
 export function ImgBgImage() {
   return (
-    <picture>
-      <source
-        media="(max-width:768px)"
-        srcSet="https://images.unsplash.com/photo-1634328724811-c20d433c3358?q=80&w=1080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        type="image/avif"
-      />
-      <source
-        media="(max-width:1200px)"
-        srcSet="https://images.unsplash.com/photo-1634328724811-c20d433c3358?q=80&w=1080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        type="image/avif"
-      />
-      <Image
-        src="https://images.unsplash.com/photo-1634328724811-c20d433c3358?q=80&w=1080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="BgImage"
-        width={500}
-        height={500}
-        sizes='"(max-width: 768px) 100vw,
-        (max-width: 1200px) 50vw,
-        33vw"'
-        priority
-        fill
-        className="h-auto w-full bg-cover bg-fixed bg-center bg-no-repeat object-cover"
-      />
-    </picture>
+    <Image
+      src="https://images.unsplash.com/photo-1634328724811-c20d433c3358?q=80&w=1080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      alt="BgImage"
+      width={500}
+      height={500}
+      sizes="100vw"
+      priority
+      fill
+      className="h-auto w-full bg-cover bg-fixed bg-center bg-no-repeat object-cover"
+    />
   )
 }
 
