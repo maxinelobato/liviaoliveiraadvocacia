ebb7a13878c0a7d355aee321437532ad8a1166a2
maxinelobato <maxinelobato89@hotmail.com>
2024-05-13 20:01:53 -0300

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -7,11 +7,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
+        hostname: '**',
       },
     ],
   },
