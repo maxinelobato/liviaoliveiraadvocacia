c81bd4aa78627d81b148e25050828dd497ec836a
maxinelobato <maxinelobato89@hotmail.com>
2024-05-18 14:03:02 -0300

    changes

--- src/app/components/Image/index.tsx
+++ src/app/components/Image/index.tsx
@@ -7,8 +7,9 @@ export function ImgBgImage() {
     <Image
       src="https://images.unsplash.com/photo-1634328724811-c20d433c3358?q=80&w=1024&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       alt="BgImage"
+      placeholder="blur"
+      quality={100}
       sizes="100vw"
-      priority
       fill
       className="h-auto w-full bg-cover bg-fixed bg-center bg-no-repeat object-cover"
     />
