0c5a2f163a9581f723735e481f971f004012118a
maxinelobato <maxinelobato89@hotmail.com>
2024-06-13 11:23:28 -0300

    changes

--- src/app/components/SectionColor/BgGradient.tsx
+++ src/app/components/SectionColor/BgGradient.tsx
@@ -10,7 +10,7 @@ export function BgGradientColor(props: BgGradientColorProps) {
   return (
     <>
       <div
-        className="min-h-full bg-gradient-to-r from-brandBlue900 via-brandBlue500 to-brandBlue500/60 bg-fixed"
+        className="min-h-full bg-gradient-to-r from-brandBlue900 via-brandBlue500 to-brandBlue500/60"
         {...props}
       />
     </>
