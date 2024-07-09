007bb240222c20ce389bfe0e0b472f2fc24d087a
maxinelobato <maxinelobato89@hotmail.com>
2024-07-09 17:08:51 -0300

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from 'next'
 import { Montserrat } from 'next/font/google'
 import './globals.css'
-import { GoogleTagManager } from '@next/third-parties/google'
+import Script from 'next/script'
+// import { GoogleTagManager } from '@next/third-parties/google'
+const GTM_ID = 'GTM-KVN3F3HK'
 
 const montserrat = Montserrat({
   subsets: ['latin', 'latin-ext'],
@@ -22,8 +24,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <GoogleTagManager gtmId="GTM-KVN3F3HK" />
+      {/* <GoogleTagManager gtmId="GTM-KVN3F3HK" /> */}
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+      </Script>
       <body className={`${montserrat.variable} scroll-smooth antialiased`}>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+          }}
+        />
         {children}
       </body>
     </html>
