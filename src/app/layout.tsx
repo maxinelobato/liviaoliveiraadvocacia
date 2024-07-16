5c7a0615d345e619fbd9115d224ac6c9aec476ce
maxinelobato <maxinelobato89@hotmail.com>
2024-07-16 18:22:35 -0300

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Script id="google-tag-manager" strategy="worker">
+      <Script id="google-tag-manager" strategy="afterInteractive">
         {`
         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
