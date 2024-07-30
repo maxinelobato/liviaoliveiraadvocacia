3f2ee498560b7c3892dad4be7f6a80b4f3c68831
maxinelobato <maxinelobato89@hotmail.com>
2024-07-30 19:25:29 -0300

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
@@ -11,6 +10,16 @@ const nextConfig = {
         hostname: '**',
       },
     ],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      })
+    }
+    return config
   },
 }
 
